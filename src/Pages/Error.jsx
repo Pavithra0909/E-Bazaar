9956e1f1b1c51be8a8bd2727b0ccf9da52d1d6dd
Pavithra0909 <mca2023.p05cc23s126008.pavithra@canaracollege.org>
2025-03-01 12:02:24 +0530

    Alt message modified

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -22,7 +22,7 @@ export default function Error() {
       {/* Error Illustration */}
       <img
         src={ErrorImage}
-        alt="hello Error gfggg"
+        alt="Alt message is changed"
         style={{ maxWidth: "400px", width: "100%", marginBottom: "20px" }}
       />
 
